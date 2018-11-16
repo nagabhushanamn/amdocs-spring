fa3b3f5654a308a72f927b5838aa76f03d629337
Nag <nagabhushanamn@gmail.com>
2018-11-16 18:23:05 +0530

    ppt

---
+++ PPT/3-spring-cloud-fundamentals/03-configuring-services-using-distributed-configuration/configuring-services-using-distributed-configuration-exercise-files/Test.java
@@ -0,0 +1,14 @@
+
+import javax.crypto.Cipher;
+
+class Test {
+  public static void main(String[] args) {
+    try {
+      System.out.println("Hello World!");
+      int maxKeyLen = Cipher.getMaxAllowedKeyLength("AES");
+      System.out.println(maxKeyLen);
+    } catch (Exception e){
+      System.out.println("Sad world :(");
+    }
+  }
+}
---
+++ PPT/3-spring-cloud-fundamentals/03-configuring-services-using-distributed-configuration/configuring-services-using-distributed-configuration-exercise-files/demos/config-client-app-with-refresh/src/main/java/io/schultz/dustin/ConfigClientAppApplication.java
@@ -0,0 +1,37 @@
+package io.schultz.dustin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+@RefreshScope
+public class ConfigClientAppApplication {
+	
+	@Autowired
+	private ConfigClientAppConfiguration properties;
+	
+	@Value("${some.other.property}")
+	private String someOtherProperty;
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientAppApplication.class, args);
+	}
+	
+	@RequestMapping
+	public String printConfig() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(properties.getProperty());
+		sb.append(" || ");
+		sb.append(someOtherProperty);
+		
+		return sb.toString();
+	}
+}
